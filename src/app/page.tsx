4e8d3f0f7a35b1c0c63744322a1814b1fb79d507
ssacharya <ssacharya2002@gmail.com>
2023-09-28 20:14:30 +0530

    api fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,15 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Card from "./components/Card";
 
-export default function Home() {
+export default  function  Home() {
+
   return (
     <>
       <main>
         <Header />
 
         <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+          
           <Card />
         </div>
       </main>
