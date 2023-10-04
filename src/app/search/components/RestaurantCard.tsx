d1ac8ab564b4010b192c676720947b7c462953ee
Shaktishankar Acharya <ssacharya2002@gmail.com>
2023-10-05 00:52:49 +0530

    filter option added

--- src/app/search/components/RestaurantCard.tsx
+++ src/app/search/components/RestaurantCard.tsx
@@ -1,29 +1,43 @@
+import RestaurantsPrice from "@/app/components/RestaurantsPrice";
+import { Location, PRICE, Region } from "@prisma/client";
 import Link from "next/link"
 
+interface restaurnatProp {
+  location: Location
+  region:Region
+  id: number;
+  name: string;
+  main_image: string;
+  slug: string;
+  price: PRICE;
+}
 
-function RestaurantCard() {
+function RestaurantCard({restaurant}: {restaurant:restaurnatProp}) {
   return (
     <div className="border-b flex pb-5">
     <img
-      src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
+      src={restaurant.main_image}
       alt=""
       className="w-44 rounded"
     />
     <div className="pl-5">
-      <h2 className="text-3xl">Aiāna Restaurant Collective</h2>
+      <h2 className="text-3xl">{restaurant.name}</h2>
       <div className="flex items-start">
         <div className="flex mb-2">*****</div>
         <p className="ml-2 text-sm">Awesome</p>
       </div>
       <div className="mb-9">
         <div className="font-light flex text-reg">
-          <p className="mr-4">$$$</p>
-          <p className="mr-4">Mexican</p>
-          <p className="mr-4">Ottawa</p>
+          <p className="mr-4">
+            <RestaurantsPrice price={restaurant.price} />
+          </p>
+          <p className="mr-4">{restaurant.region.name}</p>
+          <p className="mr-4">{restaurant.location.name}</p>
         </div>
       </div>
       <div className="text-red-600">
-        <Link href="/restaurant/cnf">View more information</Link>
+        
+        <Link href={`/restaurant/${restaurant.slug}`}>View more information</Link>
       </div>
     </div>
   </div>
--- src/app/search/components/Sidebar.tsx
+++ src/app/search/components/Sidebar.tsx
@@ -1,43 +1,94 @@
+"use client";
+import { Location, PRICE, Region } from "@prisma/client";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
+function Sidebar({
+  locations,
+  regions,
+  searchParams,
+}: {
+  locations: Location[];
+  regions: Region[];
+  searchParams: { city?: string; region?: Region; price?: PRICE };
+}) {
+  const price = [
+    {
+      price: PRICE.CHEAP,
+      label: "$",
+      className: "border w-full text-center text-reg font-light rounded-l p-2",
+    },
+    {
+      price: PRICE.REGULAR,
+      label: "$$",
+      className:
+        "border-r border-t border-b w-full text-center text-reg font-light p-2",
+    },
+    {
+      price: PRICE.EXPENSIVE,
+      label: "$$$",
+      className:
+        "border-r border-t border-b w-full text-center text-reg font-light p-2 rounded-r",
+    },
+  ];
 
-function Sidebar() {
   return (
     <div className="w-1/5">
-          <div className="border-b pb-4">
-            <h1 className="mb-2">Region</h1>
-            <p className="font-light text-reg">Toronto</p>
-            <p className="font-light text-reg">Ottawa</p>
-            <p className="font-light text-reg">Montreal</p>
-            <p className="font-light text-reg">Hamilton</p>
-            <p className="font-light text-reg">Kingston</p>
-            <p className="font-light text-reg">Niagara</p>
-          </div>
-          <div className="border-b pb-4 mt-3">
-            <h1 className="mb-2">Cuisine</h1>
-            <p className="font-light text-reg">Mexican</p>
-            <p className="font-light text-reg">Italian</p>
-            <p className="font-light text-reg">Chinese</p>
-          </div>
-          <div className="mt-3 pb-4">
-            <h1 className="mb-2">Price</h1>
-            <div className="flex">
-              <button className="border w-full text-reg font-light rounded-l p-2">
-                $
-              </button>
-              <button
-                className="border-r border-t border-b w-full text-reg font-light p-2"
-              >
-                $$
-              </button>
-              <button
-                className="border-r border-t border-b w-full text-reg font-light p-2 rounded-r"
-              >
-                $$$
-              </button>
-            </div>
-          </div>
+      <div className="border-b pb-4">
+        <h1 className="mb-2">Location</h1>
+        {locations.map((location) => (
+          <Link
+            href={{
+              pathname: "/search",
+              query: {
+                ...searchParams,
+                city: location.name,
+              },
+            }}
+            className="font-light text-reg block"
+          >
+            {location.name}
+          </Link>
+        ))}
+      </div>
+      <div className="border-b pb-4 mt-3">
+        <h1 className="mb-2">Region</h1>
+        {regions.map((region) => (
+          <Link
+            href={{
+              pathname: "/search",
+              query: {
+                ...searchParams,
+                region: region.name,
+              },
+            }}
+            className="font-light text-reg block"
+          >
+            {region.name}
+          </Link>
+        ))}
+      </div>
+      <div className="mt-3 pb-4">
+        <h1 className="mb-2">Price</h1>
+        <div className="flex">
+          {price.map(({ price, label, className }) => (
+            <Link
+              href={{
+                pathname: "/search",
+                query: {
+                  ...searchParams,
+                  price,
+                },
+              }}
+              className={className}
+            >
+              {label}
+            </Link>
+          ))}
         </div>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
